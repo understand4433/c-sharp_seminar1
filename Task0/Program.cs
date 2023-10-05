187e01cfee55bb0b8ecd3d3500c45a15616314d6
yanepumba <adress>
2023-10-05 20:29:25 +0300

    первое задание

---
+++ Task0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат 
+// (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.Write("Введите число => ");
+int inputData = int.Parse(Console.ReadLine());
+int result = inputData * inputData;
+Console.WriteLine("Квадрат числа " + inputData + " равен " + result);
+Console.WriteLine($"Квадрат числа {inputData} равен {result}");
