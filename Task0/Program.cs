d9e8f2d03a29518069995b5e5e2786f163099507
yanepumba <adress>
2023-10-05 20:51:08 +0300

    0задание

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -8,5 +8,4 @@
 Console.Write("Введите число => ");
 int inputData = int.Parse(Console.ReadLine());
 int result = inputData * inputData;
-Console.WriteLine("Квадрат числа " + inputData + " равен " + result);
 Console.WriteLine($"Квадрат числа {inputData} равен {result}");
---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого. 
+// a = 5; b = 25 -> да 
+// a = 2 b = 10 -> нет 
+// a = 9; b = -3 -> нет 
+// a = -3 b = 9 -> да
+
+Console.WriteLine("Введите первое число: ");
+int inputData0 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int inputData1 = int.Parse(Console.ReadLine());
+if (inputData1 == inputData0 * inputData0)
+
+Console.WriteLine("Является");
+else Console.WriteLine("Не является");
+
